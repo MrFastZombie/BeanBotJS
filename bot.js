d63772e4a23b6dd0df516e056fd42efe911ca2e8
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2020-03-05 15:37:16 -0600

    added basics to main script

---
+++ bot.js
@@ -0,0 +1,28 @@
+const {CommandoClient} = require('discord.js-commando');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const dtoken = process.env.DISCORD_TOKEN;
+
+
+const client = new CommandoClient({
+    commandPrefix: 'beanbot ',
+    owner: '197406355026345994',
+    invite: '',
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['fun', 'fun commands'],
+        ['utility', 'useful commands'],
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+
+client.once('ready', () => {
+    console.log('Logged in')
+});
+
+client.login(dtoken);
