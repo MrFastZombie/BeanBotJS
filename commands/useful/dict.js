5ee4b6a1542b42f9936c1a7404ba5d23b7d5cb0d
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2020-05-04 00:39:38 -0500

    Still not working, need to test on main

--- commands/useful/dict.js
+++ commands/useful/dict.js
@@ -27,6 +27,7 @@ module.exports = class DictCommand extends Command {
     }
     run(message, { wsearch }) {
         async function main() {
+			var wIndex = 0;
 			var dictResult = Owlbotclient.define(wsearch).then(function(result) {
 				if(!(typeof result === 'object')) {
 					message.say('you have broken me you fool');
@@ -35,15 +36,19 @@ module.exports = class DictCommand extends Command {
 						const owlEmbed = new RichEmbed()
 						.setColor('#0099ff')
 						.setTitle(wsearch)
-						.setDescription(result.definitions[0].definition)
-						.addField('Word type', result.definitions[0].type)
+						.setDescription(result.definitions[wIndex].definition)
+						.addField('Word type', result.definitions[wIndex].type)
 						.addField('Pronunciation', result.pronunciation)
-						.setImage(result.definitions[0].image_url)
+						.addField('Definition index', wIndex+1 + ' of ' + result.definitions.length)
+						.setImage(result.definitions[wIndex].image_url)
 						.setFooter('Dictionary services courtesy of OwlBot API')
 					message.say(owlEmbed);
-					var filter = message.content.includes('next'||'previous');
-					const collector = message.channel.createMessageCollector(filter, { time: 30000 });
+					var filter = message = message.content.includes('next'||'previous');
+					const collector = message.createMessageCollector(filter, { time: 30000 });
 					message.say("If you see this message, you can say next or previous to cycle through definitions.");
+					collector.on('collect', message => {
+						message.say('wow i collected properly wooo');
+					});
 					//await sleep(30000); //Wait 30 seconds for the user to input a next or previous command.
 				}
 			});
