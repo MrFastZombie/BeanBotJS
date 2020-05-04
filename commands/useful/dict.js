037919084a0c56869e47b08c5c112b94ad7b0f2e
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2020-05-04 00:08:07 -0500

    Commit not finished Saved to test on main branch

--- commands/useful/dict.js
+++ commands/useful/dict.js
@@ -4,13 +4,17 @@ const dotenv = require('dotenv').config();
 const Owlbot = require('owlbot-js');
 const Owlbotclient = Owlbot(process.env.OWLBOT_TOKEN);
 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 module.exports = class DictCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'dict',
             group: 'useful',
             memberName: 'dict',
-            description: 'Defines the input word',
+            description: 'Defines the input word. If there are multiple definitions, you can cycle through them by saying next and previous.',
 			examples: ['beanbot dict word','beanbot dict beans'],
 			args: [
 				{
@@ -37,6 +41,10 @@ module.exports = class DictCommand extends Command {
 						.setImage(result.definitions[0].image_url)
 						.setFooter('Dictionary services courtesy of OwlBot API')
 					message.say(owlEmbed);
+					var filter = message.content.includes('next'||'previous');
+					const collector = message.channel.createMessageCollector(filter, { time: 30000 });
+					message.say("If you see this message, you can say next or previous to cycle through definitions.");
+					//await sleep(30000); //Wait 30 seconds for the user to input a next or previous command.
 				}
 			});
         }
