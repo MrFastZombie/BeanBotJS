f30347d91e0cc4c3e6cc7da4074175d4e77e72bd
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2021-03-09 18:40:40 -0600

    gou and vbean no longer need a wait command

--- commands/fun/vbean.js
+++ commands/fun/vbean.js
@@ -5,10 +5,6 @@ const path = require('path');
 const ffmpeg = require('ffmpeg-static');
 const opusscript = require('@discordjs/opus');
 
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 module.exports = class VBeanCommand extends Command {
     constructor(client) { 
         super(client, {
@@ -22,17 +18,15 @@ module.exports = class VBeanCommand extends Command {
     async run(message) {
         const sfile = path.join(__dirname, '../../data/beaned.mp3');
         if(message.member.voice.channel != undefined) {
-            console.log("");
             const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play(sfile);
-            await sleep(8000);
+            const dispatcher = connection.play(fs.createReadStream(sfile));
             dispatcher.on('finish', () => {
-                
+                if(message.guild.me.voice.channel != undefined) {
+                    message.guild.me.voice.channel.leave();
+                }
+                dispatcher.destroy();
             });
-            dispatcher.destroy();
-            if(message.guild.me.voice.channel != undefined) {
-                message.guild.me.voice.channel.leave();
-            }
+            dispatcher.on('error', console.error);
         }
     }
 };
